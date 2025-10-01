ab364707dae0e700778228681b3630b2c9491b06
Ainhoa <agutgar3003@g.educaand.es>
2025-10-01 18:03:50 +0200

    Calculadora con el minimo redimensionado.

--- src/main/java/com/example/calculadora/HelloApplication.java
+++ src/main/java/com/example/calculadora/HelloApplication.java
@@ -12,8 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 375, 590);
-        stage.setMinHeight(375);
-        stage.setMinWidth(590);
+        stage.setMinHeight(590);
+        stage.setMinWidth(375);
         stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
